7e57995294b2f6a11230ab737c27b6bfbbba2cef
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-05-24 19:14:21 +0200

    Added UI controlls

--- src/com/grabarski/mateusz/Main.java
+++ src/com/grabarski/mateusz/Main.java
@@ -1,15 +1,26 @@
 package com.grabarski.mateusz;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Mateusz Grabarski on 21.05.2018.
  */
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 500, 375));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-        Maze maze = new Maze("maze");
-
-        System.out.println(maze.toString());
-
-        new MazeSolver(maze).solve();
+        launch(args);
     }
 }
---
+++ src/com/grabarski/mateusz/controllers/Controller.java
@@ -0,0 +1,11 @@
+package com.grabarski.mateusz.controllers;
+
+/**
+ * Created by Mateusz Grabarski on 24.05.2018.
+ */
+public class Controller {
+
+    public void initialize() {
+
+    }
+}
